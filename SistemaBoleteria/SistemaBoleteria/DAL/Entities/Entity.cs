ae025e6b260fbce699f3f8000b0cd147fe7dc691
Daniel Zapata <DZAPATAR30@GMAIL.COM>
2023-04-01 23:46:52 -0500

    Changed_Guid_to_int
    We changed the ID datatype to int, to allow easier test

--- SistemaBoleteria/SistemaBoleteria/DAL/Entities/Entity.cs
+++ SistemaBoleteria/SistemaBoleteria/DAL/Entities/Entity.cs
@@ -1,6 +1,12 @@
-﻿namespace SistemaBoleteria.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaBoleteria.DAL.Entities
 {
     public class Entity
     {
+        [Key]
+        [Required]
+        public  int Id { get; set; }
+
     }
 }
