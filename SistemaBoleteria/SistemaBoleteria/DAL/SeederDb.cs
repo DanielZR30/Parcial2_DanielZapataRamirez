060bd336599e61e933c608c736de014407ddf6bd
Daniel Zapata <DZAPATAR30@GMAIL.COM>
2023-04-02 01:30:30 -0500

    SeederDB
    We used Bogus to Generate the 50000, with Bogus we can add some rules to each field that we want to control

--- SistemaBoleteria/SistemaBoleteria/DAL/SeederDb.cs
+++ SistemaBoleteria/SistemaBoleteria/DAL/SeederDb.cs
@@ -1,6 +1,35 @@
-﻿namespace TicketSystem.DAL
+﻿using TicketSystem.DAL.Entities;
+using Bogus;
+
+namespace TicketSystem.DAL
 {
     public class SeederDb
     {
+        private readonly DatabaseContext _context;
+
+        public SeederDb(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await PopulateTicketsAsync();
+        }
+
+        private async Task PopulateTicketsAsync()
+        {
+            if(!_context.Tickets.Any())
+            {
+                //Create some rules for the tickets
+                var ticket = new Faker<Ticket>();
+
+                var tickets = ticket.Generate(50000);
+
+                _context.Tickets.AddRange(tickets);
+                _context.SaveChanges();
+            }
+        }
     }
 }
